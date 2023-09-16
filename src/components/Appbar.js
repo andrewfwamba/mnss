7b52996499942bdd9ffa00cbd0f42ca8e174d673
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-09-16 14:16:13 +0300

    links

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -22,8 +22,8 @@ function Appbar() {
   ];
   return (
     <div>
-      <nav className="py-3 md:py-5 fixed top-0 w-full flex px-4 md:px-32 justify-between items-center shadow-lg bg-white/90 z-10">
-        <div className="hidden md:block">
+      <nav className="py-3 lg:py-5 fixed top-0 w-full flex px-4 lg:px-32 justify-between items-center shadow-lg bg-white/90 z-10">
+        <div className="hidden lg:block">
           <ul className="flex space-x-4">
             {Menus.map((menu, index) => (
               <li
@@ -38,7 +38,7 @@ function Appbar() {
         {/* Mobile menu */}
         <div>
           {/* Mobile menu */}
-          <div className="block md:hidden py-2">
+          <div className="block lg:hidden py-2">
             <div onClick={() => setOpen(!open)}>
               {open ? (
                 <svg
@@ -77,7 +77,9 @@ function Appbar() {
 
         <div>
           <a
-            href="/"
+            href="https://forms.gle/XQRJ4kCnRjAoeRkRA"
+            target="_blank"
+            rel="noreferrer"
             className="w-fit px-3 py-2 rounded text-[14px] md:text-[16px] hover:bg-orange-500  shadow-lg bg-green-600 transition transform motion-reduce:transition-none motion-reduce:hover:transform-none hover:scale-105 text-gray-100"
           >
             BECOME A SPONSOR
@@ -85,12 +87,12 @@ function Appbar() {
         </div>
       </nav>
       {open && (
-        <div className="top-[4rem] md:hidden fixed z-10 uppercase w-full bg-green-600">
+        <div className="top-[4rem] lg:hidden fixed z-10 uppercase w-full bg-green-600">
           <ul className="flex px-10 flex-col mt-5 w-full text-gray-200 font-[500]">
             {Menus.map((menu, index) => (
               <li
                 key={index}
-                className="flex space-x-3 justify-between items-center cursor-pointer hover:border-b-2 pb-4 border-blue-700/50"
+                className="flex space-x-3 justify-between items-center cursor-pointer hover:border-b-2 pb-4 border-green-900/50"
               >
                 <a href={`#${menu.id}`}>
                   <p>{menu.title}</p>
